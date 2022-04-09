b7077ee9586f0aca96c491329b6236079ef3597d
devoloper-rakib <rakibhasansohag133@gmail.com>
2022-04-09 12:33:34 +0600

    new Updated

---
+++ index.js
@@ -0,0 +1,30 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(event) {
+	showSlides((slideIndex += event));
+}
+
+function currentSlide(event) {
+	showSlides((slideIndex = event));
+}
+
+function showSlides(event) {
+	let x;
+	let slides = document.getElementsByClassName('mySlides');
+	let dots = document.getElementsByClassName('dot');
+	if (event > slides.length) {
+		slideIndex = 1;
+	}
+	if (event < 1) {
+		slideIndex = slides.length;
+	}
+	for (x = 0; x < slides.length; x++) {
+		slides[x].style.display = 'none';
+	}
+	for (x = 0; x < dots.length; x++) {
+		dots[x].className = dots[x].className.replace(' active', '');
+	}
+	slides[slideIndex - 1].style.display = 'block';
+	dots[slideIndex - 1].className += ' active';
+}
